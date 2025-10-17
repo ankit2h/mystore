d97558835b70168fafa25607463926eac13e0ea7
Ankit Kumar <103123009@nitt.edu>
2025-10-17 23:58:10 +0530

    Update image source path for Amazon logo

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -56,7 +56,7 @@ const Head = () => {
                 className="flex items-center justify-center h-full mr-2"
               >
                 <img
-                  src="/amazon-logo-2.webp"
+                  src="/assets/amazon-logo-2.webp"
                   width={32}
                   height={32}
                   alt="amazon-logo"
